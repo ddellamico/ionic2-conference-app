f5c8ac9164814a425fa450a32c30984ad937d1ee
Damien Dell'Amico <damien.dellamico@saldiprivati.com>
2016-08-19 18:39:14 +0200

    refactor(providers): remove "hard" deps

--- app/core/providers/auth/auth-service.ts
+++ app/core/providers/auth/auth-service.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
-import { Storage, LocalStorage, Events } from 'ionic-angular';
+import { Storage, Events } from 'ionic-angular';
 import { AuthEvents } from '../../constants';
 import '../../helpers/rxjs-operators';
 import { BaseService } from '../base-service';
@@ -18,8 +18,6 @@ import { UserModel } from './user-model';
 export class AuthService extends BaseService {
 
   private loggedUser: UserModel = null;
-  private storage = new Storage(LocalStorage);
-  private jwtHelper = new JwtHelper();
   private USER_KEY: string = 'user_key';
   private TOKEN_KEY: string = 'id_token';
 
@@ -29,7 +27,7 @@ export class AuthService extends BaseService {
     return _authenticated;
   }
 
-  constructor(private http: Http, events: Events) {
+  constructor(private http: Http, private jwtHelper: JwtHelper, private storage: Storage, events: Events) {
     super(events);
   }
 
@@ -52,16 +50,17 @@ export class AuthService extends BaseService {
         return Observable.fromPromise(this.validateToken(res.access_token));
       })
       .catch((err: any) => {
-        this.logout();
         return this.handleError(err);
       });
   }
 
   public getLoggedUser(): Promise<UserModel> {
+    if (this.loggedUser && this.loggedUser._id) {
+      return Promise.resolve(this.loggedUser);
+    }
     return new Promise((resolve, reject) => {
       if (!tokenNotExpired()) {
-        reject('Invalid token');
-        this.logout();
+        return reject('Invalid token');
       }
       this.storage.get(this.USER_KEY).then((data) => {
         const _user = JSON.parse(data);
@@ -94,23 +93,25 @@ export class AuthService extends BaseService {
         return this.token(username, password);
       })
       .catch((err: any) => {
-        this.logout();
         return this.handleError(err);
       });
   }
-
-  public logout(): void {
-    this.storage.remove(this.TOKEN_KEY);
-    this.storage.remove(this.USER_KEY);
-    this.loggedUser = null;
-    this.events.publish(AuthEvents.USER_LOGOUT);
+  
+  public logout(): Observable<any> {
+    return Observable.forkJoin(
+      Observable.fromPromise(this.storage.remove(this.TOKEN_KEY)),
+      Observable.fromPromise(this.storage.remove(this.USER_KEY))
+    ).map(() => {
+      this.loggedUser = null;
+      this.events.publish(AuthEvents.USER_LOGOUT);
+    }).catch((err: any) => this.handleError(err));
   }
 
+  // TODO convert to Observable
   private validateToken(token: string): Promise<any> {
     return new Promise((resolve, reject) => {
       if (!token || this.jwtHelper.isTokenExpired(token)) {
-        this.logout();
-        Promise.reject('Invalid token');
+        return reject('Invalid token');
       }
       const decoded: any = this.jwtHelper.decodeToken(token);
       this.storage.set(this.TOKEN_KEY, token)
@@ -124,7 +125,6 @@ export class AuthService extends BaseService {
           resolve(true);
         })
         .catch((err) => {
-          this.logout();
           reject(err);
         });
     });
