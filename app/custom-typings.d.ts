f1e0261fa8d0d65880b49bd2d995db6ff1535c52
Damien Dell'Amico <damien.dellamico@saldiprivati.com>
2016-08-19 18:17:01 +0200

    chore(app): add deps for test enviroment

--- app/custom-typings.d.ts
+++ app/custom-typings.d.ts
@@ -44,3 +44,11 @@
 
 // Extra variables that live on Global that will be replaced by webpack DefinePlugin
 declare let __DEV__: boolean;
+
+// Ionic
+interface PageObj {
+  title: string;
+  component: any;
+  icon: string;
+  index?: number;
+}
