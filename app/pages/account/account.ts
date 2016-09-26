7384be4ad6c8754316cb63308c1fa342723af30c
Damien Dell'Amico <damien.dellamico@saldiprivati.com>
2016-09-26 13:49:33 +0200

    refactor(pages/account): restructuring to use ngrx

--- app/pages/account/account.ts
+++ app/pages/account/account.ts
@@ -1,31 +1,44 @@
+/**
+ * @author    Damien Dell'Amico <damien.dellamico@gmail.com>
+ * @copyright Copyright (c) 2016
+ * @license   GPL-3.0
+ */
+
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AuthService } from '../../core/providers/auth/auth.service';
-import { LoginPage } from '../login/login';
 import { UserModel } from '../../core/providers/auth/user-model';
+import { AuthStoreService } from '../../core/store/auth-store.service';
+import { Observable } from 'rxjs/Observable';
+import { ProfileComponent } from './profile.component';
 
 @Component({
-  template: require('./account.html')
+  template: `
+  <ion-header>
+    <ion-navbar>
+      <button menuToggle>
+        <ion-icon name="menu"></ion-icon>
+      </button>
+      <ion-title>Account</ion-title>
+    </ion-navbar>
+  </ion-header>
+  <ion-content class="outer-content">
+    <profile [profile]="profile$ | async"
+             (logout)="logout($event)"
+             (changePassword)="changePassword($event)"
+             (updatePicture)="updatePicture($event)">
+    </profile>
+  </ion-content>
+  `,
+  directives: [ProfileComponent]
 })
 export class AccountPage {
-  private profile: UserModel = null;
+  private profile$: Observable<UserModel>;
 
-  constructor(private nav: NavController,
-              private authService: AuthService) {
-  }
-
-  ngAfterViewInit() {
-    this.getUsername();
+  constructor(private authStoreService: AuthStoreService) {
+    this.profile$ = this.authStoreService.getCurrentUser();
   }
 
   updatePicture() {
     console.log('Clicked to update picture');
-  }
-
-  getUsername() {
-    this.authService.getLoggedUser().subscribe((profileData: UserModel) => {
-      this.profile = profileData;
-    });
   }
 
   changePassword() {
@@ -33,6 +46,6 @@ export class AccountPage {
   }
 
   logout() {
-    this.authService.logout().subscribe(() => this.nav.setRoot(LoginPage));
+    this.authStoreService.dispatchLogout();
   }
 }
---
+++ app/pages/account/profile.component.ts
@@ -0,0 +1,35 @@
+/**
+ * @author    Damien Dell'Amico <damien.dellamico@gmail.com>
+ * @copyright Copyright (c) 2016
+ * @license   GPL-3.0
+ */
+
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { UserModel } from '../../core/providers/auth/user-model';
+
+@Component({
+  selector: 'profile',
+  template: `
+  <div padding-top text-center *ngIf="profile">
+    <img src="http://www.gravatar.com/avatar?d=mm&s=140">
+    <h2>{{profile.username}}</h2>
+
+    <ion-list inset>
+      <button ion-item (click)="updatePicture.emit()">Update Picture</button>
+      <button ion-item (click)="changePassword.emit()">Change Password</button>
+      <button ion-item (click)="logout.emit()">Logout</button>
+    </ion-list>
+  </div>
+  `
+})
+
+export class ProfileComponent {
+
+  @Input() profile: UserModel;
+  @Output() updatePicture = new EventEmitter();
+  @Output() changePassword = new EventEmitter();
+  @Output() logout = new EventEmitter();
+
+  constructor() {
+  }
+}
